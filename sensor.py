095dc072524ab57d8dcf3f719b8e3bae2f5301ac
sorz <me@sorz.org>
2020-05-28 22:23:24 +0800

    Update README & copyrigt

--- sensor.py
+++ sensor.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 """Copyright (c) 2019, Douglas Otwell
+Copyright (c) 2020, Shell Chen
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
